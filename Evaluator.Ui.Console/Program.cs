8f9d21e57719e7812bfd4cec1064c2f69c244e8f
MIGUEL ANGEL OCHOA MARIAN <miguelochoa1113948@correo.itm.edu.co>
2026-03-15 16:04:56 -0500

    Evaluator Begings

---
+++ Evaluator.Ui.Console/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Evaluator.logic;
+
+Console.WriteLine("Expresions Evaluator");
+var expresion = "4*5/(4+6)";
+Console.WriteLine($"{expresion}={MyEvaluator.Evaluate(expresion)}");
---
+++ Evaluator.Ui.Windows/Form1.cs
@@ -0,0 +1,10 @@
+namespace Evaluator.Ui.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
